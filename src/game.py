f1f23634e14cfc87cc5e6e8cd724c2831c5f0e18
SoomIn <79903929+tnals655@users.noreply.github.com>
2021-11-12 18:04:06 +0900

    feat: Game class 구현 및 단위 테스트 실행

--- src/game.py
+++ src/game.py
@@ -1,16 +1,48 @@
+import random
+
+MIN_NUMBER = 1
+MAX_NUMBER = 30
+
+
 class Game:
     def __init__(self, user_name, difficulty):
+        self.answer_list = []
         self.user_name = user_name
         self.difficulty = difficulty
         self.current_pw = '000000'
-
-        self.create_password()
+        self.max_page = random.randrange(3, 7)
+        self.current_page = 1
+        self.answer_text = self.create_password()
 
     # 정답 결정
     def create_password(self):
-        return '100000'
+        for i in range(self.max_page):
+            rand_num = random.randrange(1, 10)
+
+            if self.max_page == 3:
+                rand_num = random.randrange(10, MAX_NUMBER + 1)
+            elif self.max_page == 4:
+                rand_num = random.randrange(1, 10) if i < 2 else random.randrange(10, MAX_NUMBER + 1)
+            elif self.max_page == 5:
+                rand_num = random.randrange(1, 10) if i > 0 else random.randrange(10, MAX_NUMBER + 1)
+
+            self.answer_list.append(rand_num)
+
+        result_text = ''
+
+        for num in self.answer_list:
+            result_text += str(num)
+
+        print("# answer_list:", self.answer_list)
+
+        return result_text
 
     # 정답 체크
     def check_password(self, password):
-        return password == self.current_pw
+        return password == self.answer_text
 
+
+if __name__ == '__main__':
+    game = Game('test', 'EASY')
+    print("# answer_text:", game.answer_text)
+    print("# max_page:", game.max_page)
