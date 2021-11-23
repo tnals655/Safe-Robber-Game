6095f7999a1df04baaedf3f5c703aba881e5fe15
SoomIn <79903929+tnals655@users.noreply.github.com>
2021-11-23 21:45:58 +0900

    feat: GameView 클래스 구현

--- src/game_view.py
+++ src/game_view.py
@@ -1,23 +1,32 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QLineEdit, QWidget, \
-    QPushButton, QHBoxLayout, QVBoxLayout, QDial
+    QPushButton, QHBoxLayout, QVBoxLayout, QDial, QLCDNumber
 from PyQt5.QtCore import Qt
-import game
+from game import Game
+from rank_view import RankView
 
 
 class GameView(QWidget):
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, game_data):
+        super().__init__(parent=None)
+        self.game = game_data
+        self.rank_view = QWidget()
+        self.rank_view.hide()
         self.initUI()
-        self.import_game = game.Game()
 
     def initUI(self):
         # 버튼 생성
-        self.result_edit = QLineEdit(self)
-        self.result_edit.setAlignment(Qt.AlignRight)
-        self.result_edit.setReadOnly(True)
-        self.result_edit.setText('000000')
+        #self.result_edit.setAlignment(Qt.AlignRight)
+        #self.result_edit.setReadOnly(True)
+        #self.result_edit.setText('000000')
+
+        self.result_lcd = QLCDNumber(self)
+        self.result_lcd.setDigitCount(6)
+        self.result_lcd.setFixedSize(300, 100)
+        self.result_lcd.setBaseSize(300, 100)
+        self.result_lcd.setMinimumSize(200, 100)
+        self.result_lcd.display('000000')
 
         self.dial = QDial(self)
         self.dial.setNotchesVisible(True)
@@ -32,7 +41,7 @@ class GameView(QWidget):
         # 레이아웃
         self.result_layout = QHBoxLayout()
         self.result_layout.addStretch(3)
-        self.result_layout.addWidget(self.result_edit)
+        self.result_layout.addWidget(self.result_lcd)
         self.result_layout.addStretch(3)
 
         self.ok_layout = QHBoxLayout()
@@ -48,22 +57,30 @@ class GameView(QWidget):
 
         self.setLayout(self.window2_layout)
 
-        self.setWindowTitle("한둘")  # 윈도우 크기상 두글자가 최대
+        self.setWindowTitle("금고털기 게임")  # 윈도우 크기상 두글자가 최대
         self.move(300, 300)
         self.resize(418, 418)
         self.show()  # show를 initUI에다가
 
     def button_clicked(self): #수정함
         num = self.dial.value()
-        current_num = self.import_game.goto_next(num) # game.py의 goto_next 에 현재 입력값을 넣어서 game.py의 current_pw 갱신
-        zeros = 6 - len(current_num) # 0의 개수
-        print(zeros)
-        display_pw = current_num + '0' * zeros # 130000
-        print(display_pw)
-        self.result_edit.setText(display_pw)
+        page = self.game.current_page
+        current_pw = self.game.goto_next(num)
+        zeros = 6 - len(current_pw)  # 0의 개수
+        display_pw = current_pw + '0' * zeros
+        #self.result_edit.setText(display_pw)
+        self.result_lcd.display(str(display_pw))
+        # game.py의 goto_next 에 현재 입력값을 넣어서 game.py의 current_pw 갱신
+        if page >= self.game.max_page:
+            self.game.record_time()
+            self.rank_view = RankView(self.game, success=self.game.check_password())
+            self.rank_view.show()
+            self.hide()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = GameView()
+    game_instance = Game()
+    game_instance.new_game(user_name='test', difficulty='Hard')
+    game_view = GameView(game_instance)
     sys.exit(app.exec_())
