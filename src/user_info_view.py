b7312803161d98898c8b735fed26c39e3612a690
SoomIn <79903929+tnals655@users.noreply.github.com>
2021-11-27 19:36:32 +0900

    feat: 유저 정보 입력 창에 게임 설명 추가

--- src/user_info_view.py
+++ src/user_info_view.py
@@ -1,8 +1,9 @@
 import sys
 
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication, QLabel, QLineEdit, QWidget, QRadioButton, QPushButton, QHBoxLayout, \
-    QVBoxLayout, QMessageBox
+    QVBoxLayout, QMessageBox, QTextEdit, QFrame
 from game import Game
 from game_view import GameView
 
@@ -31,6 +32,27 @@ class UserInfoView(QWidget):
         self.difficulty_label = QLabel('<난이도>', self)
         self.easy_explain_label = QLabel('※ Easy : 버튼을 눌러야 하는 횟수가 게임 창에 표시됩니다.', self)
         self.hard_explain_label = QLabel('※ Hard : 소리로만 정답을 유추해야 합니다.', self)
+
+        self.rule_label = QLabel("<게임 설명>")
+        self.rule_label.setAlignment(Qt.AlignCenter)
+
+        self.rule_edit = QTextEdit()
+        self.rule_edit.setReadOnly(True)
+        self.rule_edit.setAutoFillBackground(False)
+        self.rule_edit.setFrameStyle(QFrame.Plain)
+        self.rule_edit.setAlignment(Qt.AlignLeft)
+        self.rule_edit.setMinimumSize(350, 100)
+        self.rule_edit.append("금고털기 게임은 여러 개의 숫자를 조합해서 미리 정해진 6자리 숫자를 빨리 맞추는 게임입니다.")
+        self.rule_edit.append("")
+        self.rule_edit.append("다이얼에서 정답에 가까운 숫자로 향할수록 소리의 볼륨이 줄어듭니다.")
+        self.rule_edit.append("")
+        self.rule_edit.append("조합할 수 있는 숫자의 개수('Next' 버튼을 누르는 횟수)는 미리 정해져 있습니다.")
+        self.rule_edit.append("")
+        self.rule_edit.append("번호를 맞추는 데 성공하면 게임 진행 시간이 기록되며, 본인의 랭킹이 표시됩니다.")
+
+        self.window2_layout = QVBoxLayout()
+        self.window2_layout.addWidget(self.rule_label)
+        self.window2_layout.addWidget(self.rule_edit)
 
         self.easy_radio = QRadioButton('Easy', self)
         self.hard_radio = QRadioButton('Hard', self)
@@ -72,7 +94,12 @@ class UserInfoView(QWidget):
         self.window1_layout.addStretch(1)
         self.window1_layout.addLayout(self.start_layout)
 
-        self.setLayout(self.window1_layout)
+        self.main_layout = QHBoxLayout()
+        self.main_layout.addLayout(self.window1_layout)
+        self.main_layout.addSpacing(20)
+        self.main_layout.addLayout(self.window2_layout)
+
+        self.setLayout(self.main_layout)
 
         # 윈도우
         self.setWindowTitle("금고 털기 게임")
