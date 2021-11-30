ecb7537c6e15eeaaa1359b4e887dfb3c42663733
hyeesw <hyeeun7904@kookmin.ac.kr>
2021-11-30 11:20:35 +0900

    feat: GUI 부분을 initUI 메소드에 담음

--- src/rank_view.py
+++ src/rank_view.py
@@ -11,65 +11,68 @@ from ranking import Ranking
 class RankView(QWidget):
     def __init__(self, game_data, success, parent=None):
         super().__init__(parent)
+        self.game = game_data
         self.user_name = game_data.user_name
         self.difficulty = game_data.difficulty
         self.ranking = Ranking(self.difficulty)
         self.ranking.read_from_file()
+        self.rank_data = self.ranking.rank_data
         self.success = success
-        self.setWindowTitle('게임 결과')
-        self.setWindowIcon(QIcon('resources/icon.png'))
-        self.game = game_data
+        self.initUI()
 
+    def initUI(self):
+        # 랭킹
         self.rank_display = QTextEdit()
         self.rank_display.setReadOnly(True)
-
-        rank_data = self.ranking.rank_data
-
-        for i in range(len(rank_data)):
+        for i in range(len(self.rank_data)):
             if i < 3:
-                rank_user = rank_data[i][0]
-                rank_record = rank_data[i][1]
+                rank_user = self.rank_data[i][0]
+                rank_record = self.rank_data[i][1]
                 self.rank_display.append(str(i + 1) + ". " + rank_user + ": " + str(round(rank_record, 2)))
 
-        self.main_layout = QVBoxLayout()
-        self.main_layout.setSizeConstraint(QLayout.SetFixedSize)
-        self.main_layout.addWidget(self.rank_display)
-
-        self.main_layout.addSpacing(20)
-
-        self.result_label = QLabel("[ 성공! ]") if success else QLabel("[ 실패... ]")
+        # 공통 (성공/실패)
+        self.result_label = QLabel("[ 성공! ]") if self.success else QLabel("[ 실패... ]")
         self.result_label.setAlignment(Qt.AlignCenter)
-        self.main_layout.addWidget(self.result_label)
-
-        self.main_layout.addSpacing(20)
 
         self.name_text = QLabel("이름: " + str(self.user_name))
         self.name_text.setAlignment(Qt.AlignCenter)
+
+        self.end_button = QPushButton('종료')
+        self.end_button.clicked.connect(self.button_clicked)
+
+        # 메인 레이아웃
+        self.main_layout = QVBoxLayout()
+        self.main_layout.setSizeConstraint(QLayout.SetFixedSize)
+        self.main_layout.addWidget(self.rank_display)
+        self.main_layout.addSpacing(20)
+        self.main_layout.addWidget(self.result_label)
+        self.main_layout.addSpacing(20)
         self.main_layout.addWidget(self.name_text)
 
-        if success:
+        # 성공시 등수와 기록을 보여줌(레이아웃 추가)
+        if self.success:
             # 랭킹 데이터 복사해서 플레이어 등수 구하기
-            record_tuple = (self.user_name, game_data.time_record)
+            record_tuple = (self.user_name, self.game.time_record)
             temp_rank_data = self.ranking.rank_data.copy()
             temp_rank_data.append(record_tuple)
             temp_rank_data.sort()
-
             self.rank_label = QLabel("등수: " + str(temp_rank_data.index(record_tuple) + 1))
             self.rank_label.setAlignment(Qt.AlignCenter)
 
-            self.record_label = QLabel("기록: " + str(round(game_data.time_record, 2)))
+            self.record_label = QLabel("기록: " + str(round(self.game.time_record, 2)))
             self.record_label.setAlignment(Qt.AlignCenter)
 
             self.main_layout.addWidget(self.rank_label)
             self.main_layout.addWidget(self.record_label)
 
+        # 메인 레이아웃
         self.main_layout.addSpacing(40)
-
-        self.end_button = QPushButton('종료')
-        self.end_button.clicked.connect(self.button_clicked)
         self.main_layout.addWidget(self.end_button)
 
         self.setLayout(self.main_layout)
+
+        self.setWindowTitle('게임 결과')
+        self.setWindowIcon(QIcon('resources/icon.png'))
 
     def button_clicked(self):
         if self.success:
@@ -80,3 +83,4 @@ class RankView(QWidget):
             sys.exit()
         else:
             sys.exit()
+
